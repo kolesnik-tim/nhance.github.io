57f02db177041d329cd622eaa9b99974d5288c01
Timothy <31729676+kolesnik-tim@users.noreply.github.com>
2018-10-25 14:05:59 +0300

    fix

--- src/js/import/ui.js
+++ src/js/import/ui.js
@@ -12,3 +12,30 @@ import '../lib/selectize.min.js';
 //   });
 //   return false;
 // });
+
+//header
+var mywindow = $(window);
+var mypos = mywindow.scrollTop();
+var up = false;
+var newscroll;
+mywindow.scroll(function () {
+  if(mywindow.scrollTop() > 50){
+    $('.header').addClass('active');
+    newscroll = mywindow.scrollTop();
+    if (newscroll > mypos && !up) {
+        $('.header').stop().removeClass('scroll');
+        up = !up;
+        console.log(up);
+    } else if(newscroll < mypos && up) {
+        $('.header').stop().addClass('scroll');
+        up = !up;
+    }
+    mypos = newscroll;
+  } else {
+    $('.header').removeClass('active');
+  }
+});
+//dropdown
+$('.header__nav li').hover(function(){
+  $(this).toggleClass('active')
+})
