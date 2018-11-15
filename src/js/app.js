ca6219106312a74fe56c19c7622a0c41376e3dce
Timothy <31729676+kolesnik-tim@users.noreply.github.com>
2018-11-15 16:51:59 +0200

    fix

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,7 @@
+import './import/header';
 import './import/ui';
 import './import/parallax';
 import './import/swiper';
-import './import/header';
 // import objectFitImages from 'object-fit-images';
 
 
