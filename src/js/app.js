46f6ec16ad7f919cf8c10fc96e9d4741ef573943
Timothy <31729676+kolesnik-tim@users.noreply.github.com>
2018-12-04 15:40:51 +0200

    fix

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,7 @@
 import './import/header.js';
 import './import/ui';
 import './import/parallax';
+import './import/random';
 import './import/swiper';
 // import objectFitImages from 'object-fit-images';
 
--- src/js/import/ui.js
+++ src/js/import/ui.js
@@ -47,13 +47,6 @@ $('.menu__content .menu__item').on('click', function(){
   }
 })
 
-//random
-var parent = $(".team__content .row");
-var divs = parent.children();
-while (divs.length) {
-    parent.append(divs.splice(Math.floor(Math.random() * divs.length), 1)[0]);
-}
-
 
 
 //pricing mobile
